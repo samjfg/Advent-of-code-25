3c6adce9efd28e781dad2be3efd9d3d87f3cbdbd
samjfg <samueljfgoddard@gmail.com>
2025-12-20 18:18:00 +0000

    Day 1 challenge complete.

---
+++ Day-1/Day-1.py
@@ -0,0 +1,28 @@
+"""Safe Code"""
+
+
+def read_data(filename: str) -> list[str]:
+    """Returns a list with each line of the file as an element."""
+    with open(filename, "r") as f:
+        return [line.strip() for line in f]
+
+
+def dial_at_zero_count(file_input: list[str]) -> int:
+    """
+    Given the list of instruction returns the number 
+    of times the dial reaches zero.
+    """
+    dial_state = 50
+    zero_count = 0
+    for item in file_input:
+        dial_state += (-1) ** (item[0] == "L") * int(item[1:])
+        dial_state %= 100
+        if not dial_state:
+            zero_count += 1
+    return zero_count
+
+
+instructions = read_data("input.txt")
+
+count = dial_at_zero_count(instructions)
+print(count)
